d47d47c9be7b20082acc193013ac6b6cef273963
oliverrosenfeld <o_rosenfeld27@dublinschool.org>
2026-05-26 22:52:45 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IvyOrOut from './IvyOrOut.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<IvyOrOut />);
